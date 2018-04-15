32b497631beddd012bce6c1a94e67120f14be3df
Lee Hounshell <lee.hounshell@gmail.com>
2018-04-15 14:22:29 -0700

    data binding for search

---
+++ app/src/main/java/com/harlie/leehounshell/musicsearch/interfaces/ISearchMusic.java
@@ -0,0 +1,7 @@
+package com.harlie.leehounshell.musicsearch.interfaces;
+
+import android.view.View;
+
+public interface ISearchMusic {
+    void searchForMusic(View view);
+}
--- app/src/main/java/com/harlie/leehounshell/musicsearch/view/MainActivity.java
+++ app/src/main/java/com/harlie/leehounshell/musicsearch/view/MainActivity.java
@@ -1,6 +1,7 @@
 package com.harlie.leehounshell.musicsearch.view;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatEditText;
 import android.view.KeyEvent;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 
 import com.harlie.leehounshell.musicsearch.R;
+import com.harlie.leehounshell.musicsearch.databinding.ActivityMainBinding;
+import com.harlie.leehounshell.musicsearch.interfaces.ISearchMusic;
 import com.harlie.leehounshell.musicsearch.util.CustomToast;
 import com.harlie.leehounshell.musicsearch.util.LogHelper;
 import com.harlie.leehounshell.musicsearch.util.MusicSearchResults;
@@ -16,12 +19,12 @@ import com.harlie.leehounshell.musicsearch.view_model.MusicSearch_ViewModel;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements ISearchMusic {
     private final static String TAG = "LEE: <" + MainActivity.class.getSimpleName() + ">";
 
+    private ActivityMainBinding mBinding;
     private Bundle savedInstanceState;
     private MusicSearch_ViewModel musicSearch_viewModel;
-    private AppCompatEditText musicSearchEditText;
 
     private static boolean didWelcome;
 
@@ -30,16 +33,9 @@ public class MainActivity extends BaseActivity {
         LogHelper.v(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         this.savedInstanceState = savedInstanceState;
-        setContentView(R.layout.activity_main);
-        musicSearchEditText = findViewById(R.id.search_music_edit_text);
-        musicSearchEditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LogHelper.v(TAG, "-CLICK-");
-                enterSearchTerm(view);
-            }
-        });
         musicSearch_viewModel = ViewModelProviders.of(this).get(MusicSearch_ViewModel.class);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mBinding.setSearchMusic((ISearchMusic) this);
     }
 
     @Override
@@ -53,7 +49,8 @@ public class MainActivity extends BaseActivity {
         }
     }
 
-    private void enterSearchTerm(View view) {
+    @Override
+    public void searchForMusic(View view) {
         LogHelper.v(TAG, "-FOR THE READER-: i did not put API keys for Google Voice to Text translation here."
                 + " That would force readers to setup google services first.."
                 + " but, THIS IS A GOOD PLACE TO SEARCH USING MICROPHONE INPUT - left as an exercise for the reader");
@@ -68,8 +65,8 @@ public class MainActivity extends BaseActivity {
                 hideSoftKeyboard();
                 String searchString = musicSearchEditText.getText().toString();
                 if (searchString.trim().length() == 0) { // don't allow a whitespace search
-                    String invalidSearchTerm = getString(R.string.invalid_search_term);
-                    CustomToast.post(invalidSearchTerm);
+//                    String invalidSearchTerm = getString(R.string.invalid_search_term);
+//                    CustomToast.post(invalidSearchTerm);
                 }
                 else {
                     LogHelper.v(TAG, "onEditorAction: searchString=" + searchString);
